7731957683024bb27bd055fe4f4b9a1dbe195cdd
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-01 16:37:20 +0300

    Gonderiler: return JSX is completed by sending each post separately to Gonderi component

--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -10,6 +10,9 @@ const Gonderiler = (props) => {
     <div className="posts-container-wrapper">
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
+      {gonderiler.forEach((gonderi) => {
+        <Gonderi gonderi={gonderi} gonderiyiBegen={gonderiyiBegen} />;
+      })}
     </div>
   );
 };
